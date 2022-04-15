a4600c758f3181d09663a9965b86de9815bce6e2
yescine <bouchoucha.yassine@gmail.com>
2022-04-15 14:23:15 +0100

    card component test

--- cat-adoption/client/src/components/Card/Card.tsx
+++ cat-adoption/client/src/components/Card/Card.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import heartFilled from "../../svgs/heartFilled.svg";
+import heartOutlined from "../../svgs/heartOutlined.svg";
+import './Card.css';
 
 export interface Props {
   name: string;
@@ -9,12 +12,25 @@ export interface Props {
 }
 
 const Card = (props: Props) => {
-  const { email, name, phone } = props;
+  const { email, name, phone, isFavorite } = props;
+  const [isFavoured, setIsFavoured] = useState(isFavorite);
+
+  const toggleFavoured = () => {
+    setIsFavoured(!isFavoured);
+  };
+
   return (
     <div className="card">
       <div className="card-header">
         <img src={props.image.url} alt={props.image.alt} className="card-img" />
       </div>
+      <button onClick={toggleFavoured} className="heart">
+        {isFavoured ? (
+          <img src={heartFilled} alt="filled heart"></img>
+        ) : (
+          <img src={heartOutlined} alt="outlined heart"></img>
+        )}
+      </button>
       <div className="card-content">
         <h3>{name}</h3>
         <p>{phone}</p>
--- cat-adoption/client/src/components/Card/__test__/card.spec.tsx
+++ cat-adoption/client/src/components/Card/__test__/card.spec.tsx
@@ -1,9 +1,11 @@
 import { screen, render } from "@testing-library/react";
+import userEvents from "@testing-library/user-event";
+
 import Card, { Props as CardProps } from "../Card";
 
 const cardProp: CardProps = {
   email: "email@email.com",
-  image: {alt: "cute cat", url: "https://images.unsplash.com/photo-1562955239-78d8c23a3542?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" },
+  image: { alt: "cute cat", url: "https://images.unsplash.com/photo-1562955239-78d8c23a3542?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80" },
   isFavorite: true,
   name: "name",
   phone: "123123",
@@ -22,5 +24,18 @@ describe("Card element", () => {
 
   it("sould show image with source", () => {
     expect(screen.getByAltText(/cute cat/i).src).toBe(cardProp.image.url);
+  });
+
+  it("sould show filled heart", () => {
+    expect(screen.queryByAltText(/outlined heart/i)).not.toBeInTheDocument();
+    expect(screen.getAllByAltText(/filled heart/i)[0]).toBeInTheDocument();
+  });
+
+  it("sould toogle heats status", () => {
+    userEvents.click(screen.getByRole("button" ))
+    expect(screen.getAllByAltText(/outlined heart/i)[0]).toBeInTheDocument();
+    userEvents.click(screen.getByRole("button" ))
+    expect(screen.getAllByAltText(/filled heart/i)[0]).toBeInTheDocument();
+
   });
 });
