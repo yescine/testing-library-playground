63e4f4525a31e005767280738fec375f07d2518b
yescine <bouchoucha.yassine@gmail.com>
2022-04-15 09:28:24 +0100

    passin typing form

--- sign-up-form/src/App.test.tsx
+++ sign-up-form/src/App.test.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('should input be empty', () => {
-  render(<App />)
-  const emailField = screen.getByRole("textbox")
-  const passwordField = screen.getByLabelText("Password")
-  const passwordConfirmField = screen.getByLabelText(/confirm password/i)
+test("should input be empty", () => {
+  render(<App />);
+  const emailField = screen.getByRole("textbox");
+  const passwordField = screen.getByLabelText("Password");
+  const passwordConfirmField = screen.getByLabelText(/confirm password/i);
 
-  expect(emailField.value).toBe("")
-  expect(passwordField.value).toBe("")
-  expect(passwordConfirmField.value).toBe("")
+  expect(emailField.value).toBe("");
+  expect(passwordField.value).toBe("");
+  expect(passwordConfirmField.value).toBe("");
+});
 
+test("should type login form", () => {
+  const email = "slena@gmail.com";
+  const password = "pass!";
+  render(<App />);
+  const emailField = screen.getByRole("textbox", { name: /email/i });
+  const passwordField = screen.getByLabelText("Password");
+  const passwordConfirmField = screen.getByLabelText(/confirm password/i);
+
+  userEvent.type(emailField, email);
+  userEvent.type(passwordField, password);
+  userEvent.type(passwordConfirmField, password);
+
+  expect(emailField.value).toBe(email);
+  expect(passwordField.value).toBe(password);
+  expect(passwordConfirmField.value).toBe(password);
 });
