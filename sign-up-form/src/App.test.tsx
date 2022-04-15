a163877d356acf34b5881568d2d14c56e3edb122
yescine <bouchoucha.yassine@gmail.com>
2022-04-15 11:23:10 +0100

    enclose in describe

--- sign-up-form/src/App.test.tsx
+++ sign-up-form/src/App.test.tsx
@@ -3,10 +3,6 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-beforeEach(() => {
-  render(<App />);
-});
-
 function typeForm({ email, password, confirmPassword }: { email?: string; password?: string; confirmPassword?: string }) {
   const emailField = screen.getByRole("textbox", { name: /email/i });
   const passwordField = screen.getByLabelText("Password");
@@ -24,34 +20,44 @@ function submitForm() {
   userEvent.click(submitBtn);
 }
 
-test("should input be empty", () => {
-  expect(screen.getByRole("textbox", { name: /email/i }).value).toBe("");
-  expect(screen.getByLabelText("Password").value).toBe("");
-  expect(screen.getByLabelText(/confirm password/i).value).toBe("");
-});
+describe("fill in form", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
 
-test("should type login form", () => {
-  const email = "slena@gmail.com";
-  const password = "pass!";
-  const { emailField, passwordField, passwordConfirmField } = typeForm({ email, password, confirmPassword: password });
+  describe("safe path", () => {
+    test("should input be empty", () => {
+      expect(screen.getByRole("textbox", { name: /email/i }).value).toBe("");
+      expect(screen.getByLabelText("Password").value).toBe("");
+      expect(screen.getByLabelText(/confirm password/i).value).toBe("");
+    });
 
-  expect(emailField.value).toBe(email);
-  expect(passwordField.value).toBe(password);
-  expect(passwordConfirmField.value).toBe(password);
-});
+    test("should type login form", () => {
+      const email = "slena@gmail.com";
+      const password = "pass!";
+      const { emailField, passwordField, passwordConfirmField } = typeForm({ email, password, confirmPassword: password });
 
-test("should show error text", () => {
-  const email = "slenagmail.com";
-  typeForm({ email });
-  submitForm();
+      expect(emailField.value).toBe(email);
+      expect(passwordField.value).toBe(password);
+      expect(passwordConfirmField.value).toBe(password);
+    });
 
-  expect(screen.queryByText(/the email you input is invalid/i)).toBeInTheDocument();
-});
+    test("should not show error text", () => {
+      const email = "slena@gmail.com";
+      typeForm({ email });
+      submitForm();
 
-test("should not show error text", () => {
-  const email = "slena@gmail.com";
-  typeForm({ email });
-  submitForm();
+      expect(screen.queryByText(/the email you input is invalid/i)).not.toBeInTheDocument();
+    });
+  });
 
-  expect(screen.queryByText(/the email you input is invalid/i)).not.toBeInTheDocument();
+  describe("error path", () => {
+    test("should show error text", () => {
+      const email = "slenagmail.com";
+      typeForm({ email });
+      submitForm();
+
+      expect(screen.queryByText(/the email you input is invalid/i)).toBeInTheDocument();
+    });
+  });
 });
